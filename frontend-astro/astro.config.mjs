d2d0652efc895c3810fbf49a7c9326fcc724b246
rahul-chacko-7 <rahulchacko7796@gmail.com>
2026-03-23 16:56:20 +0000

    email added

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
   srcDir: './src',
   outDir: './dist',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [tailwind()]
 });
