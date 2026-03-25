b3ec1bcd729b9101a49f5eb90cc666ce884695ab
rahul-chacko-7 <rahulchacko7796@gmail.com>
2026-03-25 16:13:47 +0000

    email added

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   srcDir: './src',
   outDir: './dist',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [tailwind()]
 });
