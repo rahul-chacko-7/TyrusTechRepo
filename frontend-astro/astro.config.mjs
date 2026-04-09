95c3574fe9f5bd14c53ea1da84c8b6b256bcde25
rahul-chacko <rahul.chacko@iouring.com>
2026-04-09 19:53:33 +0530

    bugs fixed

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://tyrustech.com',
